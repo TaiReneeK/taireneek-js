ef0cdd6cff6687d3fc6fe8ff29d581338d96d13c
Tai-Renee <TaiReneeK09@gmail.com>
2018-07-11 17:51:22 -0400

    almost finished

--- js/app.js
+++ js/app.js
@@ -1,92 +1,65 @@
-(function()
+const projects = document.querySelectorAll('.project');
+
+for(let i=0; i<projects.length; i++){
+  let button = projects[i].querySelector('button');
+  let para = projects[i].querySelector('p');
+  
+  button.addEventListener('click', function(){
+    projects[i].classList.toggle('expanded');
+    button.innerHTML = projects[i].classList.contains('expanded') ?
+      'Hide Details' : 'Show Details';
+ });
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) 
 {
-	// setUpPage();
+  showSlides(slideIndex += n);
+}
 
-	// function setUpPage()
+// Thumbnail image controls
+function currentSlide(n) 
+{
+  showSlides(slideIndex = n);
+}
+
+function showSlides() 
+{
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000);
+}
+
+ //    var coll = document.getElementsByClassName("collapsible");
+	// var i;
+
+	// for (i = 0; i < coll.length; i++) 
 	// {
-	// 	addSliderEventListeners();
+ //  	coll[i].addEventListener("click", function() 
+ //  	{
+ //    this.classList.toggle("active");
+ //    var content = this.nextElementSibling;
+ //    if (content.style.display === "block") 
+ //    	{
+ //      content.style.display = "none";
+ //    	} 
+	// else 
+	// 	{
+ //      content.style.display = "block";
+ //    	}
+ //  	});
 	// }
-
- //  function addSliderEventListeners() 
- //  {
-
- //    const sliderPrev = document.getElementById('slider-prev');
- //    const sliderNext = document.getElementById('slider-next');
- //    const sliderFrame = document.getElementById('slider-frame');
- //    const sliderCaption = document.getElementById('slider-caption');
-
- //    const slides =
- //      Array.from(sliderFrame.querySelectorAll('div[data-src]'))
- //        .map(div => 
- //        {
- //          return 
- //          {
- //            imageUrl: div.getAttribute('data-src'),
- //            caption: div.textContent.trim(),
- //          };
- //        });
-
- //    let sliderIndex = 0;
- //    displaySliderImage();
-
- //    function displaySliderImage() 
- //    {
- //      let { imageUrl, caption } = slides[sliderIndex];
- //      sliderFrame.style.backgroundImage = `url('${imageUrl}')`;
- //      let count = `(${sliderIndex+1}/${slides.length}) `;
- //      sliderCaption.innerHTML = count + caption;
- //      sliderCaption.classList.add('flash');
- //      setTimeout(() => 
- //      {
- //        sliderCaption.classList.remove('flash');
- //      }, 0);
- //    }
-
- //    function displayPrevSliderImage() 
- //    {
- //      sliderIndex--;
- //      if (sliderIndex < 0) 
- //      {
- //        sliderIndex = slides.length - 1;
- //      }
- //      displaySliderImage();
- //    }
-
- //    function displayNextSliderImage() 
- //    {
- //      sliderIndex++;
- //      if (sliderIndex === slides.length) 
- //      {
- //        sliderIndex = 0;
- //      }
- //      displaySliderImage();
- //    }
-
- //    sliderPrev.addEventListener('click', displayPrevSliderImage);
- //    sliderNext.addEventListener('click', displayNextSliderImage);
-
- //    setInterval(function() 
- //    {
- //      displayNextSliderImage();
- //    }, 4000);
-
-    var coll = document.getElementsByClassName("collapsible");
-	var i;
-
-	for (i = 0; i < coll.length; i++) 
-	{
-  	coll[i].addEventListener("click", function() 
-  	{
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") 
-    	{
-      content.style.display = "none";
-    	} 
-	else 
-		{
-      content.style.display = "block";
-    	}
-  	});
-	}
-}();
